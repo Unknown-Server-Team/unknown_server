230681976b16b5bd55dbf3a650d8ba06478f4e89
santiadjmc <santiadjmc@gmail.com>
2025-02-10 14:46:11 -0300

    Add custom CSS for dark theme in Swagger UI

--- config/swagger.js
+++ config/swagger.js
@@ -50,4 +50,60 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 
+// Add custom CSS for dark theme
+specs.customCss = `
+    .swagger-ui {
+        background-color: #1a1a1a;
+        color: #ffffff;
+    }
+    .swagger-ui .info .title,
+    .swagger-ui .info .description,
+    .swagger-ui .scheme-container,
+    .swagger-ui table thead tr th,
+    .swagger-ui .parameters-col_name {
+        color: #ffffff;
+    }
+    .swagger-ui .opblock {
+        background: #2d2d2d;
+        border-color: #404040;
+    }
+    .swagger-ui .opblock .opblock-summary-method {
+        background: #4a4a4a;
+    }
+    .swagger-ui .opblock .opblock-summary-description,
+    .swagger-ui .parameter__name,
+    .swagger-ui .parameter__type,
+    .swagger-ui table.model tr td {
+        color: #cccccc;
+    }
+    .swagger-ui .tab li {
+        color: #ffffff;
+    }
+    .swagger-ui input[type=text],
+    .swagger-ui textarea {
+        background-color: #333333;
+        color: #ffffff;
+    }
+    .swagger-ui .topbar {
+        background-color: #2d2d2d;
+    }
+    .swagger-ui .model-box {
+        background-color: #2d2d2d;
+    }
+    .swagger-ui section.models {
+        background: #2d2d2d;
+    }
+    .swagger-ui .response-col_status {
+        color: #ffffff;
+    }
+    .swagger-ui .responses-inner h4,
+    .swagger-ui .responses-inner h5 {
+        color: #ffffff;
+    }
+    .swagger-ui .opblock-tag {
+        color: #ffffff;
+        border-color: #404040;
+    }
+`;
+
 module.exports = specs;
